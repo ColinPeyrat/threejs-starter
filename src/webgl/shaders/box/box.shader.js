8ae5a2ada5e9d87cd045c7fb7e6d2c8742e71d8e
Colin Peyrat <contact@colin-peyrat.com>
2018-02-13 23:58:53 +0100

    ✍️ Add todo

--- src/webgl/shaders/box/box.shader.js
+++ src/webgl/shaders/box/box.shader.js
@@ -2,6 +2,8 @@ import glslify from 'glslify';
 import vertexShaderSource from './box.vert';
 import fragmentShaderSource from './box.frag';
 
+// TODO npm script to automate the creation of new shaders
+
 const vertexShader = glslify(vertexShaderSource);
 const fragmentShader = glslify(fragmentShaderSource);
 
